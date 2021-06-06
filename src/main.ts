c5c0308451165fb0c674d01becaa44048c3aded2
Quint Daenen <me@di-wu.be>
2021-06-06 11:53:24 +0200

    Add option to install vessel.

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,8 @@ export async function run() {
 
         // Opt-out of having data collected about dfx usage.
         core.exportVariable('DFX_TELEMETRY_DISABLED', 1);
+
+        // Install dfx.
         cp.execSync(`echo y | DFX_VERSION=${dfxVersion} sh -ci "$(curl -fsSL https://sdk.dfinity.org/install.sh)"`);
         core.addPath('/home/runner/bin');
 
@@ -16,12 +18,23 @@ export async function run() {
         core.debug(dfxPath);
         infoExec(`${dfxPath} --version`);
 
+        // Install dfx cache to get moc.
         cp.execSync(`${dfxPath} cache install`);
         let cachePath = infoExec(`${dfxPath} cache show`).trim();
         core.addPath(cachePath);
 
         let mocPath = await io.which('moc');
         infoExec(`${mocPath} --version`);
+
+        // Install vessel.
+        let vesselVersion = core.getInput('vessel-version');
+        if (vesselVersion) {
+            cp.execSync(`curl -L https://github.com/dfinity/vessel/releases/download/v${vesselVersion}/vessel-linux64 > /home/runner/bin/vessel`);
+            cp.execSync(`chmod +x /home/runner/bin/vessel`);
+
+            let vesselPath = await io.which('vessel');
+            infoExec(`${vesselPath} --version`);
+        }
     } catch (e) {
         core.setFailed(e.message);
     }
