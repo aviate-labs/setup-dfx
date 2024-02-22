eefb595a667a27b20642a8bc36e5397453302628
byronbecker <byron.becker@colorado.edu>
2024-02-22 15:26:36 -0800

    Now that dfxvm has launched, update to install with the auto initialize environment variable set

--- src/main.ts
+++ src/main.ts
@@ -34,7 +34,7 @@ export async function run() {
         core.exportVariable('DFX_TELEMETRY_DISABLED', 1);
 
         // Install dfx.
-        cp.execSync(`echo y | DFX_VERSION=${dfxVersion} sh -ci "$(curl -fsSL https://sdk.dfinity.org/install.sh)"`);
+        cp.execSync(`echo y | DFXVM_INIT_YES=true DFX_VERSION=${dfxVersion} sh -ci "$(curl -fsSL https://sdk.dfinity.org/install.sh)"`);
 
         const dfxPath = await io.which('dfx');
         core.debug(dfxPath);
